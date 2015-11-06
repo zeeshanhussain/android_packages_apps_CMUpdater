febd7d3cb5e3bb17307b542b474f0665df10a017
Sultanxda <sultanxda@gmail.com>
2015-11-05 23:18:24 -0800

    Revert "CMUpdater: Switch to maybeTranslateEmulatedPathToInternal"
    Not using this (prepare for incoming reverts).
    
    This reverts commit 9f6b70b4b6530288bf1c17cfcc6fb254d508d307.

--- src/com/cyanogenmod/updater/utils/Utils.java
+++ src/com/cyanogenmod/updater/utils/Utils.java
@@ -138,8 +138,7 @@ public class Utils {
         // Add the update folder/file name
         String primaryStoragePath = Environment.getExternalStorageDirectory().getAbsolutePath();
         // If data media rewrite the path to bypass the sd card fuse layer and trigger uncrypt
-        String directPath = Environment.maybeTranslateEmulatedPathToInternal(
-                new File(primaryStoragePath)).getAbsolutePath();
+        String directPath = Environment.getMediaStorageDirectory().getAbsolutePath();
         String updatePath = Environment.isExternalStorageEmulated() ? directPath :
                 primaryStoragePath;
         String cmd = "echo '--update_package=" + updatePath + "/" + Constants.UPDATES_FOLDER + "/"
